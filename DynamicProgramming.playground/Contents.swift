154a28ef02860245e12d5985545cf0967c03f0ff
John N <thehtjohn@gmail.com>
2020-04-18 16:15:59 -0700

    Longest Palindromic substring

--- DynamicProgramming.playground/Contents.swift
+++ DynamicProgramming.playground/Contents.swift
@@ -48,3 +48,68 @@ var emptyDictionary: [Int: Int] = [:]
 
 fibWithMemoization(n: 10, memo: &emptyDictionary)
 
+//Corner Cases:
+// Can we assert that there is no space
+// s = "bbbab" -> ["b"], ["bb"], ["bbb"], ["bbbb"] , ["bab"] -> ["bbbb"] -> 4
+// s = "cbbd" -> ["c"], ["cb"], ["cbb"], ["cd"]
+
+// 1. Get all possible subsequences
+// 2. Get the subsequences with Palindromes
+// 3. Loop through and find the max Length
+
+let s = "bbbab"
+//bbbab
+//[bb],[bb], [ba],[ba], [bbbb]
+
+//T- 2^n S- O(n)
+func longestPalindromicSubsequence(s: String) -> Int {
+    var subSequence = ""
+    var subSequences:[String] = []
+    var palindromicSubsequences: [String] = []
+    var maxSubsequenceLength = Int.min
+    
+    getAllPossibleSubSequences(s: s, subsequence: &subSequence, subsequences: &subSequences, startIndex: 0)
+    
+    for subSequenceElement in subSequences {
+        if isPalindrome(s: subSequenceElement) {
+            palindromicSubsequences.append(subSequenceElement)
+        }
+    }
+    
+    for palindromicSubsequence in palindromicSubsequences {
+        maxSubsequenceLength = maxSubsequenceLength > palindromicSubsequence.count ? maxSubsequenceLength : palindromicSubsequence.count
+    }
+    
+    return maxSubsequenceLength
+}
+
+func getAllPossibleSubSequences(s: String, subsequence: inout String, subsequences: inout [String], startIndex: Int) -> [String] {
+    subsequences.append(subsequence)
+    for i in startIndex..<s.count {
+        subsequence.append(Array(s)[i])
+        getAllPossibleSubSequences(s: s, subsequence: &subsequence, subsequences: &subsequences, startIndex: i + 1)
+        subsequence.removeLast()
+    }
+    
+    return subsequences
+}
+
+func isPalindrome(s: String) -> Bool {
+    var startIndex = 0
+    var endIndex = s.count - 1
+    while startIndex < endIndex {
+    
+        if Array(s)[startIndex] != Array(s)[endIndex] {
+            return false
+        }
+        
+        startIndex += 1
+        endIndex -= 1
+    }
+    return true
+}
+
+longestPalindromicSubsequence(s: s)
+
+
+
