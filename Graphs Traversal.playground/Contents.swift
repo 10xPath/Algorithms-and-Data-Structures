0f8ff1add872b196f8d3a984400bbfffcaaa3037
John N <thehtjohn@gmail.com>
2020-05-25 10:59:04 -0700

    Is connected graph

--- Graphs Traversal.playground/Contents.swift
+++ Graphs Traversal.playground/Contents.swift
@@ -7,12 +7,11 @@ var str = "Hello, playground"
 //a dfs with recursion,  a dfs with stack and bfs with queue to count the number of nodes in starting from node 0.
 
 
-//MARK: DFS Recursion
+//MARK: 1.1 DFS Recursion
 
 var nodeCounterDFS = 0
 var adjListDSF:[Int:[Int]] = [:]
 func countNumberOfNodesDFSRecursion(adjList:[Int:[Int]], startingPoint: Int) -> Int {
-    nodeCounterDFS = 0
     adjListDSF = adjList
     if adjList.count == 0 {
         return 0
@@ -34,7 +33,7 @@ func nodeCounterDFSRecursion(_  node: Int) {
 
 
 
-//MARK: DFS Stack
+//MARK:1.2 DFS Stack
 
 func countNumberOfNodesDFSStack(adjList:[Int:[Int]], startingPoint: Int) -> Int {
     var nodeCounter = 0
@@ -57,7 +56,7 @@ func countNumberOfNodesDFSStack(adjList:[Int:[Int]], startingPoint: Int) -> Int 
     return nodeCounter
 }
 
-//MARK: BFS
+//MARK: 1.3 BFS
 func countNumberOfNodesBFS(adjList: [Int:[Int]], startingPoint: Int) -> Int{
     
     var nodeCounter = 0
@@ -79,5 +78,69 @@ func countNumberOfNodesBFS(adjList: [Int:[Int]], startingPoint: Int) -> Int{
     
     return nodeCounter
 }
+
+
+//MARK: 2.1
+
+func isConnectedGraphDFSRecursion(adjList:[Int:[Int]], startingPoint: Int) {
+    adjListDSF = adjList
+    
+    nodeCounterDFSRecursion(0)
+    
+    let isConnected = nodeCounterDFS == adjList.count ? "YES" : "NO"
+    
+    print(isConnected)
+}
+
+
+//MARK: 2.2
+func isConnectedGraphDFSStack(adjList:[Int:[Int]], startingPoint: Int) {
+    var nodeCounter = 0
+       
+       if adjList.count == 0 {
+           print("NO")
+       }
+       
+       var stack = [startingPoint]
+       
+       if !stack.isEmpty {
+           nodeCounter += 1
+           let currentNode = stack.removeLast()
+           let neighbors = adjList[currentNode]!
+           for neighbor in neighbors {
+               stack.append(neighbor)
+           }
+       }
+       
+    let isConnected = nodeCounter == adjList.count ? "YES" : "NO"
+    
+    print(isConnected)
+}
+
+//MARK: 2.3
+func isConnectedGraphBFS(adjList: [Int:[Int]], startingPoint: Int) {
+    var nodeCounter = 0
+       
+       if adjList.count == 0 {
+           print("NO")
+       }
+       
+       var queue = [startingPoint]
+       
+       if !queue.isEmpty {
+           nodeCounter += 1
+           let currentNode = queue.removeFirst()
+           let neighbors = adjList[currentNode]!
+           for neighbor in neighbors {
+               queue.append(neighbor)
+           }
+       }
+       
+    let isConnected = nodeCounter == adjList.count ? "YES" : "NO"
+    
+    print(isConnected)
+}
+
+
 
 
